4849110456486b9b1523f67c7176d6f05155287d
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-11-07 15:55:10 -0500

    first commit

---
+++ cloudj-backend/src/main/java/com/cloudj/backend/CloudjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cloudj.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudjBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudjBackendApplication.class, args);
+	}
+
+}
