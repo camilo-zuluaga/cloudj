cc5402adc7a91b8ff67fdb43b62e018f50cc538e
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-11-12 19:03:06 -0500

    feat: endpoints to support multipart upload with pre-signed urls

--- cloudj-backend/src/main/java/com/cloudj/backend/controller/FileController.java
+++ cloudj-backend/src/main/java/com/cloudj/backend/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.cloudj.backend.controller;
 
+import com.cloudj.backend.dto.request.CompleteMultiPartUpload;
 import com.cloudj.backend.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,10 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(
+        origins = {"http://localhost:5000", "http://127.0.0.1:5000"},
+        allowedHeaders = "*"
+)
 @RequestMapping("/files")
 public class FileController {
 
@@ -17,10 +22,40 @@ public class FileController {
 
     @PostMapping("/pre-signed-url")
     public ResponseEntity<Map<String, String>> generatePresignedURL(
-            @RequestParam("fileExtension") String fileExtension
+            @RequestParam("fileName") String fileName,
+            @RequestParam("contentType") String contentType
     ) {
-        String keyName = "%s.%s".formatted(UUID.randomUUID(), fileExtension);
-        var url = fileService.generatePresignedPutURL(keyName);
-        return ResponseEntity.ok(Map.of("url", url, "file", keyName));
+        String key = String.valueOf(UUID.randomUUID());
+        var url = fileService.generatePresignedPutURL(key, fileName, contentType);
+        return ResponseEntity.ok(Map.of("key", key, "url", url));
+    }
+
+    @PostMapping("/start-multipart")
+    public ResponseEntity<Map<String, String>> startMultipartUpload(
+            @RequestParam("fileName") String fileName,
+            @RequestParam("contentType") String contentType
+    ) {
+        String key = String.valueOf(UUID.randomUUID());
+        var uploadId = fileService.multiPartUploadID(key, fileName, contentType);
+        return ResponseEntity.ok(Map.of("key", key, "id", uploadId));
+    }
+
+    @PostMapping("{key}/pre-signed-part")
+    public ResponseEntity<Map<String, String>> generatePresignedPart(
+            @PathVariable String key,
+            @RequestParam String uploadId,
+            @RequestParam int partNumber
+    ) {
+        var url = fileService.multiPartPresignedURL(key, uploadId, partNumber);
+        return ResponseEntity.ok(Map.of("key", key, "url", url));
+    }
+
+    @PostMapping("{key}/complete-multipart-upload")
+    public ResponseEntity<Map<String, String>> completeMultipartUpload(
+            @PathVariable String key,
+            @RequestBody CompleteMultiPartUpload completeMultiPartUpload
+    ) {
+        var location = fileService.completeMultiPartUpload(key, completeMultiPartUpload);
+        return ResponseEntity.ok(Map.of("key", key, "location", location));
     }
 }
