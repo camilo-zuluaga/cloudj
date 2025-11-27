bd97255a4860738d08d34a098a9da03664b1dc46
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-11-27 15:26:29 -0500

    feat: gather user's id from the security context using @AuthenticationPrincipal for naming purposes

--- cloudj-backend/src/main/java/com/cloudj/backend/service/CustomUserDetailsService.java
+++ cloudj-backend/src/main/java/com/cloudj/backend/service/CustomUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.cloudj.backend.service;
 
+import com.cloudj.backend.domain.User;
+import com.cloudj.backend.dto.request.CustomUserDetails;
 import com.cloudj.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,7 +17,9 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user.getId(), user.getEmail(), user);
     }
 }
