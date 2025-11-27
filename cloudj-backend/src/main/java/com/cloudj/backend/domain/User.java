b21875f7c3e759daa28672eb279e46bd08f94849
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-11-27 15:19:09 -0500

    fix: lazy fetch to avoid n+1 calls

--- cloudj-backend/src/main/java/com/cloudj/backend/domain/User.java
+++ cloudj-backend/src/main/java/com/cloudj/backend/domain/User.java
@@ -32,7 +32,7 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
