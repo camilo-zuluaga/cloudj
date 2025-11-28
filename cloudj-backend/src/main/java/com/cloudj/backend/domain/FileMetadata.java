c512cbd70bdd145ab14b7b3b8dbf9dc7ccbadc3b
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-11-27 19:33:12 -0500

    feat: change relationship of file metadata

--- cloudj-backend/src/main/java/com/cloudj/backend/domain/FileMetadata.java
+++ cloudj-backend/src/main/java/com/cloudj/backend/domain/FileMetadata.java
@@ -33,8 +33,8 @@ public class FileMetadata {
     @Column(length = 150, nullable = false)
     private String contentType;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public FileMetadata(String s3Key, String originalFilename, Long fileSize, LocalDateTime uploadedAt, String contentType, User user) {
