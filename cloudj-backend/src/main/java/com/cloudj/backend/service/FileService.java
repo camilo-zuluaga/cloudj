94795cab6cdbae53a2c2d2f28e9098c2a31e8217
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-11-27 19:34:18 -0500

    feat: file service to separate use cases

---
+++ cloudj-backend/src/main/java/com/cloudj/backend/service/FileService.java
@@ -0,0 +1,39 @@
+package com.cloudj.backend.service;
+
+import com.cloudj.backend.domain.FileMetadata;
+import com.cloudj.backend.domain.User;
+import com.cloudj.backend.dto.out.MessageResponse;
+import com.cloudj.backend.dto.request.CompletedSingleUpload;
+import com.cloudj.backend.repository.FileMetadataRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FileService {
+
+    private final FileMetadataRepository metadataRepository;
+
+    @Transactional
+    public MessageResponse saveMetadata(User user, CompletedSingleUpload request) {
+        FileMetadata metadata = FileMetadata.builder()
+                .user(user)
+                .s3Key(request.keyName())
+                .originalFilename(request.fileName())
+                .contentType(request.contentType())
+                .fileSize(request.fileSize())
+                .uploadedAt(LocalDateTime.now())
+                .build();
+
+        metadataRepository.save(metadata);
+        return new MessageResponse("File metadata saved", LocalDateTime.now());
+    }
+
+    public List<FileMetadata> getUserFiles(Long id) {
+        return metadataRepository.findFilesByUserId(id);
+    }
+}
