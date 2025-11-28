7e36f7a3d9d06b6ba7dfa6921853d9984e37e02a
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-11-27 19:33:54 -0500

    feat: native query to avoid unnecessary dto and mapping

--- cloudj-backend/src/main/java/com/cloudj/backend/repository/FileMetadataRepository.java
+++ cloudj-backend/src/main/java/com/cloudj/backend/repository/FileMetadataRepository.java
@@ -2,6 +2,12 @@ package com.cloudj.backend.repository;
 
 import com.cloudj.backend.domain.FileMetadata;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FileMetadataRepository extends JpaRepository<FileMetadata, Long> {
+    @Query(value = "SELECT * FROM file_metadata WHERE user_id = :id", nativeQuery = true)
+    List<FileMetadata> findFilesByUserId(@Param("id") Long id);
 }
