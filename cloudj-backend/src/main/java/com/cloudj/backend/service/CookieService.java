98309371875098bd2babe2c28d5e3d691ba1efe4
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-12-20 16:51:37 -0500

    feat: cookie builder method and deletion

--- cloudj-backend/src/main/java/com/cloudj/backend/service/CookieService.java
+++ cloudj-backend/src/main/java/com/cloudj/backend/service/CookieService.java
@@ -1,30 +1,34 @@
 package com.cloudj.backend.service;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CookieService {
 
     public void addHttpOnlyCookie(String name, String value, int maxAge, HttpServletResponse response) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(maxAge);
+        ResponseCookie cookie = ResponseCookie.from(name, value)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .path("/")
+                .maxAge(maxAge)
+                .build();
 
-        response.addCookie(cookie);
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
     }
 
     public void deleteCookie(String name, HttpServletResponse response) {
-        Cookie cookie = new Cookie(name, null);
+        ResponseCookie cookie = ResponseCookie.from(name, null)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .path("/")
+                .maxAge(0)
+                .build();
 
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(0);
-
-        response.addCookie(cookie);
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
     }
 }
