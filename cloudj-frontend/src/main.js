7a6987cb5beacaeb945938705acc73ab1f283831
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-12-20 17:03:17 -0500

    feat: app and main

---
+++ cloudj-frontend/src/main.js
@@ -0,0 +1,20 @@
+import "./assets/main.css"
+
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+import App from "./App.vue"
+import router from "@/router/index.js"
+
+import "vuetify/styles"
+import { createVuetify } from "vuetify"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
+
+const app = createApp(App)
+const pinia = createPinia()
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+app.use(router).use(pinia).use(vuetify).mount("#app")
